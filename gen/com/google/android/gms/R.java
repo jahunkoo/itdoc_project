e24badd7b93961f45eaed81aa5054beea39dacb6
jahunkoo <koo10682@gmail.com>
2014-08-08 22:52:42 +0900

    koo

--- gen/com/google/android/gms/R.java
+++ gen/com/google/android/gms/R.java
@@ -65,10 +65,10 @@ public final class R {
 		public static final int common_signin_btn_text_normal_light = 0x7f020073;
 		public static final int common_signin_btn_text_pressed_dark = 0x7f020074;
 		public static final int common_signin_btn_text_pressed_light = 0x7f020075;
-		public static final int ic_plusone_medium_off_client = 0x7f020087;
-		public static final int ic_plusone_small_off_client = 0x7f020088;
-		public static final int ic_plusone_standard_off_client = 0x7f020089;
-		public static final int ic_plusone_tall_off_client = 0x7f02008a;
+		public static final int ic_plusone_medium_off_client = 0x7f02009e;
+		public static final int ic_plusone_small_off_client = 0x7f02009f;
+		public static final int ic_plusone_standard_off_client = 0x7f0200a0;
+		public static final int ic_plusone_tall_off_client = 0x7f0200a1;
 	}
 	public static final class id {
 		public static final int hybrid = 0x7f05001c;
