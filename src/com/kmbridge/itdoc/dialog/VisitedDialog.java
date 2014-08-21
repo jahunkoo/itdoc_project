a8a106f7b1b1272503b6ce95a801c3c2925d725a
kkm1234 <vgte14@naver.com>
2014-08-22 03:14:39 +0900

    asda

--- src/com/kmbridge/itdoc/dialog/VisitedDialog.java
+++ src/com/kmbridge/itdoc/dialog/VisitedDialog.java
@@ -1,15 +1,14 @@
 package com.kmbridge.itdoc.dialog;
 
-import junit.framework.Test;
 import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kmbridge.itdoc.R;
 
@@ -26,6 +25,13 @@ public class VisitedDialog extends Dialog implements OnClickListener {
 	private TextView item2;
 	private TextView item3;
 	private TextView item4;
+	
+	boolean check_item0 = true;
+	boolean check_item1 = true;
+	boolean check_item2 = true;
+	boolean check_item3 = true;
+	boolean check_item4 = true;
+	
 	private Button btnDigComplete;
 	
 	String strColor = "#DBC000";
@@ -35,8 +41,8 @@ public class VisitedDialog extends Dialog implements OnClickListener {
 	public VisitedDialog(Context context,View.OnClickListener clicklistener) {
 		super(context);
 		//requestWindowFeature(Window.FEATURE_NO_TITLE);
-		this.setCanceledOnTouchOutside(false);// 다이알로그 바깥영역 터치시, 다이알로그 닫히지 않기
-		this.setCancelable(true); // 백키로 다이알로그 닫기
+		//this.setCanceledOnTouchOutside(false);// 다이알로그 바깥영역 터치시, 다이알로그 닫히지 않기
+		//this.setCancelable(true); // 백키로 다이알로그 닫기
 
 		mContext = context;
 		mClickListener = clicklistener;
@@ -76,19 +82,44 @@ public class VisitedDialog extends Dialog implements OnClickListener {
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.dialog_item0:
-			setDlgLayout("한방 다이어트",item0);
+			if(check_item0==true)
+			{
+				setDlgLayout("한방 다이어트",item0);
+				check_item0=false;
+			}
+			else Toast.makeText(mContext, "이미 등록된 키워드 입니다.", Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.dialog_item1:
-			setDlgLayout("불임",item1);
+			if(check_item1==true)
+			{
+				setDlgLayout("불임",item1);
+				check_item1=false;
+			}
+			else Toast.makeText(mContext, "이미 등록된 키워드 입니다.", Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.dialog_item2:
-			setDlgLayout("변비",item2);
+			if(check_item2==true)
+			{
+				setDlgLayout("변비",item2);
+				check_item2=false;
+			}
+			else Toast.makeText(mContext, "이미 등록된 키워드 입니다.", Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.dialog_item3:
-			setDlgLayout("피부",item3);
+			if(check_item3==true)
+			{
+				setDlgLayout("피부",item3);
+				check_item3=false;
+			}
+			else Toast.makeText(mContext, "이미 등록된 키워드 입니다.", Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.dialog_item4:
-			setDlgLayout("탈모",item4);
+			if(check_item4==true)
+			{
+				setDlgLayout("탈모",item4);
+				check_item4=false;
+			}
+			else Toast.makeText(mContext, "이미 등록된 키워드 입니다.", Toast.LENGTH_SHORT).show();
 			break;
 			
 		case R.id.btn_dialog_complete:
