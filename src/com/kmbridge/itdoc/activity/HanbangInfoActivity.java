f621a3a4ebbadf0c281a466a4c8dd30cfc445431
jahunkoo <koo10682@gmail.com>
2014-08-26 02:49:30 +0900

    koo

--- src/com/kmbridge/itdoc/activity/HanbangInfoActivity.java
+++ src/com/kmbridge/itdoc/activity/HanbangInfoActivity.java
@@ -1,9 +1,11 @@
 package com.kmbridge.itdoc.activity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.widget.TextView;
 
 import com.kmbridge.itdoc.R;
 import com.kmbridge.itdoc.fragment.HanBangInnerFragment;
@@ -14,6 +16,15 @@ public class HanbangInfoActivity extends FragmentActivity{
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hanbang_info);
+		
+		TextView text0 = (TextView) findViewById(R.id.textview_hanbang_info_0);
+		TextView text1 = (TextView) findViewById(R.id.textview_hanbang_info_1);
+		TextView text2 = (TextView) findViewById(R.id.textview_hanbang_info_2);
+		TextView text3 = (TextView) findViewById(R.id.textview_hanbang_info_3);
+		text0.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/nanum_bold.ttf"));
+		text1.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/nanum_bold.ttf"));
+		text2.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/nanum_bold.ttf"));
+		text3.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/nanum_bold.ttf"));
 		//getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		
 		/*Bundle bundle = getIntent().getExtras();
